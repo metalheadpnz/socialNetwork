fb18e617bd8f0c666ab459f6084d6d75ef0c908e
metalheadpnz <metalheadpnz@gmail.com>
2020-05-12 01:19:33 +0300

    Lesson 28. state

---
+++ src/redux/state.js
@@ -0,0 +1,38 @@
+let state = {
+    MainContentData: {
+        ProfileData: {
+            PostListData: {
+                postsData: [
+                    {id: 1, message: 'First post', likeCounter: 15},
+                    {id: 1, message: 'First post', likeCounter: 15},
+                    {id: 1, message: 'чета', likeCounter: 10},
+                    {id: 1, message: 'Metallica', likeCounter: 100500},
+                    {id: 1, message: 'nothing to commit', likeCounter: 666},
+                    {id: 1, message: 'Ну хватит уже', likeCounter: 13}
+                ]
+            }
+        },
+        DialogsData: {
+            DialogsListData: {
+                usersDialogsData: [
+                    {id: 1, userName: 'Vasyan'},
+                    {id: 2, userName: 'Boryan'},
+                    {id: 3, userName: 'Mixan'},
+                    {id: 4, userName: 'Mixan'},
+                    {id: 5, userName: 'Mixan'}
+                ]
+            },
+            MessagesListData: {
+                messagesData: [
+                    {id: 1, message: 'Message 1'},
+                    {id: 2, message: 'Message 2'},
+                    {id: 3, message: 'Message 3'},
+                    {id: 4, message: 'Message 4'},
+                    {id: 5, message: 'Message 5'},
+                ]
+            }
+        }
+    }
+}
+
+export default state;
