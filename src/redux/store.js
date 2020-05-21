57ab3b67d10481863dc7b0a3615d168c05d3d634
metalheadpnz <metalheadpnz@gmail.com>
2020-05-21 13:29:46 +0300

    Lesson 41. reducer

---
+++ src/redux/store.js
@@ -0,0 +1,65 @@
+import ProfileReducer from "./ProfileReducer";
+import DialogsReducer from "./DialogsReducer";
+
+
+
+let store = {
+    _state: {
+        MainContentData: {
+            ProfileData: {
+                PostsListData: {
+                    PostItemData: [
+                        {id: 1, message: 'First post', likeCounter: 15}
+                    ]
+                },
+                AddPostFormData: {
+                    textareaData: 'начальное значение в state',
+                }
+            },
+            DialogsData: {
+                DialogsListData: {
+                    usersDialogsData: [
+                        {id: 1, userName: 'Vasyan'},
+                        {id: 2, userName: 'Boryan'},
+                        {id: 3, userName: 'Mixan'},
+                        {id: 4, userName: 'Mixan'},
+                        {id: 5, userName: 'Mixan'}
+                    ]
+                },
+                MessagesListData: {
+                    messagesData: [
+                        {id: 1, message: 'Message 1'}
+                    ]
+                },
+                SendMessageFormData: {
+                    newMessageText: 'Значение из стейт'
+                }
+            }
+        },
+        SideBarData: {
+            FriendsPanelSmall: [
+                {id: 1, userName: 'Vasyan'},
+                {id: 2, userName: 'Boryan'},
+                {id: 3, userName: 'Mixan'}]
+        },
+    },
+    _callSubscriber() {
+    },
+
+    getState() {
+        return (this._state);
+    },
+    subscribe(observer) {
+        store._callSubscriber = observer;
+    },
+    dispatch(action) {
+        this._state.MainContentData.ProfileData = ProfileReducer(this._state.MainContentData.ProfileData, action);
+        this._state.MainContentData.DialogsData = DialogsReducer(this._state.MainContentData.DialogsData, action);
+        this._callSubscriber(this._state);
+    }
+}
+
+export default store;
+
+
+
