d51d3219182fbd54fcd89626a95def7ec8388d24
metalheadpnz <metalheadpnz@gmail.com>
2020-07-03 22:45:07 +0300

    Lesson 83. reselect

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,7 +1,17 @@
-export const getUsers = (state) => {
-        return state.UsersPage.users.filter (f => true)
-        // return state.UsersPage.users
+// export const getUsers = (state) => {
+//         return state.UsersPage.users.filter (f => true)
+//         // return state.UsersPage.users
+// }
+
+import {createSelector} from "reselect";
+
+export const getUsersSelector = (state) => {
+    return state.UsersPage.users
 }
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+        return users.filter(u => true);
+    })
 
 export const getPagination = (state) => {
     return state.UsersPage.pagination
